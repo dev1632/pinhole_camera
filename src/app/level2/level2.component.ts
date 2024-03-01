52bed017c7fb273fb55c344e60a049277868e8e6
Jiggs29 <singhjigyashu@gmail.com>
2024-03-02 01:57:33 +0530

    Partial Running of the Level 3

--- src/app/level2/level2.component.ts
+++ src/app/level2/level2.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-level2',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./level2.component.css']
 })
 export class Level2Component {
+  username: string = '';
+  constructor(private route: ActivatedRoute, private router: Router) {   
+  }
+
+  next(){ 
+      this.router.navigate(['/level1/level2/level3'], { queryParams: { username: this.username }});
+  }
 
 }
