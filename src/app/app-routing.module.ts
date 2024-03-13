b5e6e435ef08ac27359329db8a8323874ab440d0
Deven <devenpwagh@gmail.com>
2024-03-13 16:39:38 +0530

    hello

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import { Level3Component } from './level3/level3.component';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'level1', component: Level1Component },
-  { path: 'level1/level2', component: Level2Component },
+  { path: 'level2', component: Level2Component },
   { path: 'level1/level2/level3', component: Level3Component },
 ];
 
